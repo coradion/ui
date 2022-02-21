33211030d8de054f196395795689e03fd1343349
Bayan Bennett <16971062+BayanBennett@users.noreply.github.com>
2022-02-20 19:23:20 -0800

    chore(storybook): build with webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,5 +8,8 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
 }

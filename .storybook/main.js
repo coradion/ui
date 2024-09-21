3deb06020f564f9c276df8a79e5669bfddd2cb8b
Bayan Bennett <16971062+BayanBennett@users.noreply.github.com>
2024-09-20 22:29:48 -0700

    chore: migrate to storybook 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -13,13 +14,19 @@ const config = {
         },
       },
     },
+    "@storybook/addon-webpack5-compiler-swc",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,7 +7,6 @@ import { withThemeByClassName } from "@storybook/addon-styling";
 import "../src/styles.css";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
